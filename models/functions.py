f63701f6f56d1e502309a89e643b1329cd070ee0
ibrahima Faye <ibouddha1.if@gmail.com>
2024-12-17 19:20:09 +0000

    latest stable version

--- models/functions.py
+++ models/functions.py
@@ -3,15 +3,17 @@ from flask import jsonify
 import requests
 from datetime import datetime
 import json
+import os
+from dotenv import load_dotenv
 
-
-conn = connect()
+load_dotenv()
 
 def crawl():
+    conn = connect()
     data = []
     if conn is not None:
         occurences = []
-        api_key = '741ed36b784c1106f493ba003bc8f39d'
+        api_key = os.getenv('api_key')
         lat = 14.6937
         lon = -17.444059
         # start = 1369789200
@@ -24,7 +26,7 @@ def crawl():
             ville = data["city"]["name"]
             for datum in list_data:
                 temp = int(datum["main"]["temp"] - 273.75)
-                timestamp = datum["dt_txt"]
+                timestamp = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
                 occurence = {
                     timestamp :{
                     "name": ville,  # Access the first element
@@ -44,15 +46,15 @@ def crawl():
     else:
        data = "Problems with Database"
     
-    return datum
+    return data
 
 def insert(data):
+    conn = connect()
     if conn is not None:
         cur = conn.cursor()
         for timestamp, weather_data in data.items():
             cur.execute("INSERT INTO meteo (timestamp, ville, temperature, description, pression, humidite) VALUES (%s, %s, %s, %s, %s, %s)", (timestamp, weather_data["name"], weather_data["temperature"], weather_data["description"], weather_data["pression"], weather_data["humidity"]))
         conn.commit()
         cur.close()
-        # conn.close()
     else:
         print("Database connection failed")
