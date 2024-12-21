20b8e6d2565b063016efb44ce64ee0b1bfc38074
ibrahima Faye <ibouddha1.if@gmail.com>
2024-12-21 15:35:56 +0000

    latest version - with thies

--- models/functions.py
+++ models/functions.py
@@ -1,8 +1,8 @@
 from models.connect import connect
-from flask import jsonify
+# from flask import jsonify
 import requests
 from datetime import datetime
-import json
+# import json
 import os
 from dotenv import load_dotenv
 
@@ -14,35 +14,37 @@ def crawl():
     if conn is not None:
         occurences = []
         api_key = os.getenv('api_key')
-        lat = 14.6937
-        lon = -17.444059
-        # start = 1369789200
-        # end = 1629233600
-        url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            list_data = data['list']
-            ville = data["city"]["name"]
-            for datum in list_data:
-                temp = int(datum["main"]["temp"] - 273.75)
-                timestamp = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
-                occurence = {
-                    timestamp :{
-                    "name": ville,  # Access the first element
-                    "temperature": temp,
-                    "description": datum["weather"][0]["description"],
-                    "pression": datum["main"]["pressure"],
-                    "humidity": datum["main"]["humidity"]
+        coordonnées = {"Dakar":[14.6937,-17.444059], "Thies":[14.6583066,-17.4378091]}
+        lat = 0
+        lon = 0
+        for city, coord in coordonnées.items():
+            lat = coord[0]
+            lon = coord[1]
+            url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                list_data = data['list']
+                ville = city
+                for datum in list_data:
+                    temp = int(datum["main"]["temp"] - 273.75)
+                    timestamp = datum['dt_txt']
+                    occurence = {
+                        timestamp :{
+                        "name": ville,  # Access the first element
+                        "temperature": temp,
+                        "description": datum["weather"][0]["description"],
+                        "pression": datum["main"]["pressure"],
+                        "humidity": datum["main"]["humidity"]
+                        }
                     }
-                }
-                insert(occurence)
+                    insert(occurence)
+                    
+                    occurences.append(occurence)
                 
-                occurences.append(occurence)
-            
-            return occurences
-        else:
-            data = f"Problems with API response {response.status_code}" 
+            else:
+                data = f"Problems with API response {response.status_code}" 
+        return occurences
     else:
        data = "Problems with Database"
     
